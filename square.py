5e6b520aa8323dbb8cf5f8bb3201d0ce76668ca1
Abraham Ho Ming Tsang <tsangabr@IITS-B473-02.utsc-labs.utoronto.ca>
2017-09-26 11:32:30 -0400

    added general power function

--- square.py
+++ square.py
@@ -4,7 +4,19 @@ def square(x):
     result += x
   return result
 
+def power(x, y):
+  if x == 0:
+    return 0
+  if y == 0:
+    return 1
+  result = 0
+  for i in range(x):
+    result += power(x, y-1)
+  return result
+
 if __name__ == "__main__":
   for i in range(5):
     print("The square of %s is %s" % (i, square(i)))
-
+  for i in range(5):
+    for j in range(4):
+      print ("%s to the %sth power is %s" % (i, j, power(i, j)))
