342120ba5bc17f9f9bf6ab893a8848cf44c09343
Chengli Yang <yangch86@IITS-B473-03.utsc-labs.utoronto.ca>
2017-09-26 11:33:01 -0400

    Cube

--- square.py
+++ square.py
@@ -7,4 +7,11 @@ def square(x):
 if __name__ == "__main__":
   for i in range(5):
     print("The square of %s is %s" % (i, square(i)))
+	print("The cube of %s is %s", i, cube(i))
+
+def cube(x):
+  result = square(x) * x
+  return result
+
+
 
